1a7467f917a84d764132c53cf3ce7bb8f0955c28
susufufu-XHD <14750323@me.mcu.edu.tw>
2026-04-29 12:03:37 +0800

    week07

---
+++ week07/week07-3b.cpp
@@ -0,0 +1,10 @@
+//week07-3b.cpp SOIT107_Base_017
+# include <stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d", &a, &b);
+    int ans = a - b * 6;
+    printf("%d %d", ans/6, ans%6);
+
+    }
